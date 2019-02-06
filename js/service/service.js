9b64049b3b98f973362b850473474f6e41e08945
Dakota <dakota.vradenburg@gmail.com>
2019-02-05 19:23:20 -0500

    Attempting to fix img render issue

--- js/service/service.js
+++ js/service/service.js
@@ -6,21 +6,21 @@ function PortfolioService() {
     self.projects = [
         {
             name: "The HEARD",
-            img: "images/the-heard-project.jpg",
+            img: "../images/the-heard-project.jpg",
             url: "https://the-heard.herokuapp.com/#!/about",
             github: "https://github.com/DakotaV6/the-herd",
             description: "An interactive web app built with Spotify API and AngularJS."
         },
         {
             name: "The Back Door",
-            img: "images/back-door-snap.png",
+            img: "../images/back-door-snap.png",
             url: "https://dakotav6.github.io/the-back-door-project/#home",
             github: "https://github.com/DakotaV6/the-back-door-project",
             description: "Shopping cart project using HTML, CSS, JavaScript, & jQuery."
         },
         {
             name: "Flashing Lights",
-            img: "images/angular-project-snap.png",
+            img: "../images/angular-project-snap.png",
             url: "https://dakotav6.github.io/event-search-project-ng/#!/",
             github: "https://github.com/DakotaV6/event-search-project-ng",
             description: "Event search application using TicketMaster API and AngularJS."
